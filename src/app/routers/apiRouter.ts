521bbad46d20cd6facebeb2e8eb8b314f77ca425
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-20 18:46:34 +0300

    Update apiRouter.ts

--- src/app/routers/apiRouter.ts
+++ src/app/routers/apiRouter.ts
@@ -29,12 +29,31 @@ router.post("/calculate", async (req, res) => {
   try {
     const { formData }: { formData: WebsiteFormData } = req.body;
 
+    if ((await User.find({ email: formData.email })).length > 0)
+      return res.status(400).json({
+        message: `This email is assosiated with an existing caphub account.
+             Please Login to the platform to continue
+            or use a different email address.`,
+      });
+
     const newUser = new User({
       email: formData.email,
       name: formData.email,
       passwordHash: "unset",
     });
     await newUser.save();
+
+    const {
+      annualRevenue,
+      currency,
+      annualGrowthRate,
+      currentRunway,
+      termLength,
+      gracePeriod,
+      email,
+    } = formData;
+
+    const arr = parseFloat(annualRevenue);
 
     res.status(200).json({ result: 234234 });
   } catch (error) {
