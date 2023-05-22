86a8f57ed5cf776463edaf9c1c2d84fe2104f442
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-23 00:47:02 +0300

    Update authRouter.ts

--- src/app/routers/authRouter.ts
+++ src/app/routers/authRouter.ts
@@ -13,53 +13,15 @@ import { v4 as keyv4 } from "uuid";
 const router = express.Router();
 const MIN_PASSWORD_STRENGTH = 3;
 
-router.post("/signin", async (req, res) => {
+router.get("/signedin", async (req, res) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password)
-      res.status(400).json({ clientError: "Wrong email or password" });
-
-    const existingUser = await User.findOne({ email });
-    if (!existingUser)
-      return res.status(401).json({
-        clientError: "Wrong email or password",
-      });
-
-    const correctPassword = await bcrypt.compare(
-      password,
-      existingUser.passwordHash
-    );
-
-    if (!correctPassword)
-      return res.status(401).json({
-        clientError: "Wrong email or password",
-      });
-
-    const token = jwt.sign(
-      {
-        id: existingUser._id,
-      },
-      process.env.JWT_SECRET as string
-    );
-
-    res
-      .cookie("jwt", token, {
-        httpOnly: true,
-        sameSite:
-          process.env.NODE_ENV === "development"
-            ? "lax"
-            : process.env.NODE_ENV === "production" && "none",
-        secure:
-          process.env.NODE_ENV === "development"
-            ? false
-            : process.env.NODE_ENV === "production" && true,
-      })
-      .send();
+    const token = req.cookies.jwt;
+    if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
+    const userId = (validatedUser as JwtPayload).id;
+    res.json(await User.findById(userId));
   } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ serverError: "Unexpected error occurred in the server" });
+    return res.status(401).json({ errorMessage: "Unauthorized." });
   }
 });
 
@@ -118,8 +80,6 @@ router.post("/signupfin", async (req, res) => {
         .json({ clientError: "Invalid or expired signup link" });
     }
 
-    const MIN_PASSWORD_STRENGTH = 3;
-
     const passwordStrength = zxcvbn(password);
 
     if (passwordStrength.score < MIN_PASSWORD_STRENGTH)
@@ -138,10 +98,7 @@ router.post("/signupfin", async (req, res) => {
       return res.status(400).json({
         clientError: "An account with this email already exists",
       });
-    if (!existingSignupRequest)
-      return res.status(400).json({
-        clientError: "The key is wrong",
-      });
+
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     const savedUser = await new User({
@@ -149,12 +106,14 @@ router.post("/signupfin", async (req, res) => {
       name: fullname,
       passwordHash,
     }).save();
+
     const token = jwt.sign(
       {
         id: savedUser._id,
       },
       process.env.JWT_SECRET as string
     );
+
     res
       .cookie("jwt", token, {
         httpOnly: true,
@@ -176,55 +135,53 @@ router.post("/signupfin", async (req, res) => {
   }
 });
 
-router.get("/signedin", async (req, res) => {
+router.post("/signin", async (req, res) => {
   try {
-    const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
-    const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
-    const userId = (validatedUser as JwtPayload).id;
-    res.json(await User.findById(userId));
-  } catch (err) {
-    return res.status(401).json({ errorMessage: "Unauthorized." });
-  }
-});
+    const { email, password } = req.body;
+    if (!email || !password)
+      res.status(400).json({ clientError: "Wrong email or password" });
 
-router.post("/updatename", async (req, res) => {
-  try {
-    const token = req.cookies.jwt;
-    const { name } = req.body;
-    if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
-    const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
-    const userId = (validatedUser as JwtPayload).id;
-    const user = await User.findById(userId);
-    if (user) user.name = name;
-    await user?.save();
-    res.json(user);
-  } catch (err) {
-    return res.status(401).json({ errorMessage: "Unauthorized." });
-  }
-});
-
-router.post("/updatepassword", async (req, res) => {
-  try {
-    const token = req.cookies.jwt;
-    const { password } = req.body;
-    const passwordStrength = zxcvbn(password);
-    if (passwordStrength.score < MIN_PASSWORD_STRENGTH)
-      return res.status(400).json({
-        clientError:
-          "Password isn't strong enough, the value is" + passwordStrength.score,
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(401).json({
+        clientError: "Wrong email or password",
       });
-    if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
-    const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
-    const userId = (validatedUser as JwtPayload).id;
-    const user = await User.findById(userId);
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-    if (user) user.passwordHash = passwordHash;
-    await user?.save();
-    res.json(user);
+
+    const correctPassword = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+
+    if (!correctPassword)
+      return res.status(401).json({
+        clientError: "Wrong email or password",
+      });
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      process.env.JWT_SECRET as string
+    );
+
+    res
+      .cookie("jwt", token, {
+        httpOnly: true,
+        sameSite:
+          process.env.NODE_ENV === "development"
+            ? "lax"
+            : process.env.NODE_ENV === "production" && "none",
+        secure:
+          process.env.NODE_ENV === "development"
+            ? false
+            : process.env.NODE_ENV === "production" && true,
+      })
+      .send();
   } catch (err) {
-    return res.status(401).json({ errorMessage: "Unauthorized." });
+    console.error(err);
+    res
+      .status(500)
+      .json({ serverError: "Unexpected error occurred in the server" });
   }
 });
 
@@ -328,29 +285,43 @@ router.post("/passresfin", async (req, res) => {
   }
 });
 
-router.post("/updaten", async (req, res) => {
+router.post("/updatename", async (req, res) => {
   try {
-    const { notifications, newsletter } = req.body;
-    if (
-      (notifications !== true && notifications !== false) ||
-      (newsletter !== true && newsletter !== false)
-    )
-      return res.status(400).json({
-        clientError: "At least one of the fields are missing",
-      });
-
     const token = req.cookies.jwt;
+    const { name } = req.body;
     if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
     const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
     const userId = (validatedUser as JwtPayload).id;
-    const user = (await User.find({ userId }))[0];
-    await user.save();
-    res.json({ changed: "yes" });
+    const user = await User.findById(userId);
+    if (user) user.name = name;
+    await user?.save();
+    res.json(user);
   } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ serverError: "Unexpected error occurred in the server" });
+    return res.status(401).json({ errorMessage: "Unauthorized." });
+  }
+});
+
+router.post("/updatepassword", async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    const { password } = req.body;
+    const passwordStrength = zxcvbn(password);
+    if (passwordStrength.score < MIN_PASSWORD_STRENGTH)
+      return res.status(400).json({
+        clientError:
+          "Password isn't strong enough, the value is" + passwordStrength.score,
+      });
+    if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET as string);
+    const userId = (validatedUser as JwtPayload).id;
+    const user = await User.findById(userId);
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    if (user) user.passwordHash = passwordHash;
+    await user?.save();
+    res.json(user);
+  } catch (err) {
+    return res.status(401).json({ errorMessage: "Unauthorized." });
   }
 });
 
